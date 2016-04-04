cb1a9c1faf1e885485bbe0df97a3d910af66fb86
TK_Sourabh <sourabhtk37@gmail.com>
2016-04-04 16:02:29 +0530

    Rungee kutte program gave correct ouput

--- rungee_kutte/main.cpp
+++ rungee_kutte/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include<math.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  double x1=0,y1=1,y2,k,k1,k2,k3,k4,h=0.1;
+
+  for(int i=0;i<2;i++)
+  {
+     k1=h*(x1+pow(y1,2));
+     k2=h*(x1+(h/2) + pow(y1+(k1/2),2));
+     k3=h*(x1+(h/2) + pow(y1+(k2/2),2));
+     k4=h*(x1+h+pow((y1+k3),2));
+
+     k=(k1 + 2*k2 + 2*k3 + k4)/6;
+
+     y2=y1+k;
+     x1=x1+h;
+     y1=y2;
+  }
+
+  cout<<"y= "<<y2;
 }
